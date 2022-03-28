df1941238b55cc47a431164662dd952bbc45d5de
Andrew <andslobby@gmail.com>
2022-03-28 18:24:05 +0300

    doc: Add config2 for tester

---
+++ config/config2.py
@@ -0,0 +1,21 @@
+# Default visualisation ID
+CLIENT_ID = 'ДНЦ'
+
+# Default visualisation host IP address
+CLIENT_HOST = '10.23.101.167'
+
+# Default visualisation host TCP port
+# hex or dec are allowed
+CLIENT_PORT = 5556
+
+# Dictionary of clients
+
+CLIENTS = {
+    CLIENT_ID: (CLIENT_HOST, CLIENT_PORT),
+    # 'ШН': ('localhost',5555),
+}
+
+# Station name
+# used for opening proper stations/station_name/description.csv file
+
+STATION = 'tester'
